618ebb06dc002bedac1e71f3de94f11f4352f512
Dennis Fridrich <fridrich.dennis@gmail.com>
2022-08-27 19:08:24 +0200

    Add SK IBAN example, fix typos and code issues

---
+++ public/test.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Defr\QRPlatba\QRPlatba;
+
+$qrPlatba = new QRPlatba();
+
+$qrPlatba->setAccount('12-3456789012/0100')
+    ->setVariableSymbol('2016001234')
+    ->setMessage('Toto je první QR platba.')
+    ->setConstantSymbol('0308')
+    ->setSpecificSymbol('1234')
+    ->setAmount('1234.56')
+    ->setCurrency('CZK') // Výchozí je CZK, lze zadat jakýkoli ISO kód měny
+    ->setDueDate(new \DateTime());
+
+echo $qrPlatba->getQRCodeImage(); // Zobrazí <img> tag s kódem, viz níže
